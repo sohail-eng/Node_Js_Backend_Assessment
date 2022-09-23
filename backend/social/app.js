2a49695cad6c20ce87e64adbb76574cc42c35904
sohail-eng <sohailamjad865@gmail.com>
2022-09-23 11:35:15 +0500

    Update
    Add Post Get Patch And Delete Request And Database configuration

--- backend/social/app.js
+++ backend/social/app.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 const express = require('express');
 // const { initConsumer } = require('./utilities/consumer');
-const { initProducer } = require('./utilities/producer');
+// const { initProducer } = require('./utilities/producer');
 // const { connectConsumer } = require('./utilities/consumer');
 // const { connectProducer, connectAdmin } = require('./utilities/producer');
 // const KeyMaster = require('./utilities/KeyMaster');
-// const databaseConfig = require('./database/DatabaseConfig');
+ const databaseConfig = require('./database/DatabaseConfig');
 
 const app = express();
 
@@ -13,15 +15,116 @@ app.use(express.json());
 
 // app.use(databaseConfig.initializeDB());
 
-app.use('/', async (req, res) => {
+//app.use('/', async (req, res) => {
 
-	res.status(200).json({ message: `App is running on port. ${process.env.PORT || 4000}` });
+	//res.status(200).json({ message: `App is running on port. ${process.env.PORT || 4000}` });
 
-});
+//});
+
+app.post('/tenant_profie',(req,res)=>
+{
+	databaseConfig('Tenant_Profile').insert(
+		req.body
+	).then((_id)=>
+	{
+		res.send({"Result":"Record Inserted "});
+		console.log(_id);
+	});
+})	
+
+app.post('/user_profile',(req,res)=>
+{
+	databaseConfig('User_Profile').insert(
+		req.body
+	).then((_id)=>
+	{
+		res.send({"Result":"Record Inserted "});
+		console.log(_id);
+	});
+})	
+
+
+app.get('/tenant_profie',(req,res)=>
+{
+	databaseConfig.select().from('Tenant_Profile').then((users)=>
+		{
+			res.json(users);
+		});	
+})	
+
+
+app.get('/user_profile',(req,res)=>
+{
+	databaseConfig.select().from('User_Profile').then((users)=>
+		{
+			res.json(users);
+		});	
+})
+
+app.get('/tenant_profie/:id',(req,res)=>
+{
+	databaseConfig.select().from('Tenant_Profile').where("tenant_id",req.params.id).then((users)=>
+	{
+		res.json(users);
+	});
+})
+
+app.get('/user_profile/:id',(req,res)=>
+{
+	databaseConfig.select().from('User_Profile').where("user_id",req.params.id).then((users)=>
+	{
+		res.json(users);
+	});	
+})
+
+app.delete('/tenant_profie/:id',(req,res)=>
+{
+	databaseConfig('Tenant_Profile').where("tenant_id",req.params.id).del().then((count)=>
+	{
+		res.send({"Result":"Deleted Records : "+ count});
+	});
+})
+
+app.delete('/user_profile/:id',(req,res)=>
+{
+	databaseConfig('User_Profile').where("user_id",req.params.id).del().then((count)=>
+	{
+		res.send({"Result":"Deleted Records : "+ count});
+	});
+})
+
+app.patch('/tenant_profie/:id',(req,res)=>
+{
+	databaseConfig('Tenant_Profile').where("tenant_id",req.params.id).update(req.body).then((count)=>
+	{
+		res.send({"Result":"Updated Records : "+ count});
+	});
+})
+
+app.patch('/user_profile/:id',(req,res)=>
+{
+	databaseConfig('User_Profile').where("user_id",req.params.id).update(req.body).then((count)=>
+	{
+		res.send({"Result":"Updated Records : "+ count});
+	});
+})
+
+async function  check_params( json, paramlist)
+{
+	var jsd={};
+
+	await paramlist.forEach(element => {
+		if(!json[element])json[element]="";
+		jsd[element]=json[element];
+		console.log(element);
+	});
+	return jsd;
+}
 
 app.listen(process.env.PORT || 4000, async () => {
 	
 	console.log('App started at port', process.env.PORT || 4000);
-	await initProducer();
-
+	//console.log(databaseConfig);
+	
+	//await initProducer();
 });
---
+++ backend/social/database/DatabaseConfig.js
@@ -0,0 +1,11 @@
+
+
+const knex = require('knex');
+const knexfile = require('./knexfile');
+const db=knex(knexfile.development);
+module.exports = db;
+
+module.exports.initializeDB=()=>
+{
+    
+}
